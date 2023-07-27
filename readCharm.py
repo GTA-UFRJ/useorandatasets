88fd2c9ecd2dad0ca9910ebe5f6f10a5c9019077
rodsouzacouto <49560685+rodsouzacouto@users.noreply.github.com>
2023-07-27 15:07:44 -0300

    Add files via upload

--- readCharm.py
+++ readCharm.py
@@ -3,21 +3,26 @@
 
 import numpy as np
 
-def read_iq_values_from_binary_file(filename,num_elements):
-    # Read the binary file into a numpy array of 32-bit floats
-    data = np.fromfile(filename, dtype=np.float32, count=num_elements)
+def read_iq_values_from_binary_file(filename,nrSamples):
+    # Read the binary file into a numpy array
+    # dtype='<f4' means 32-bit floats with little endian
+    data = np.memmap(filename, dtype='<f4', mode='r')
+
+    #Select a subset fromt the data, based on the number of required samples
+    datasubset = data[0:nrSamples*2]
 
     # Reshape the data to split I and Q values into separate columns
-    iq_data = data.reshape(-1, 2)
+    iq_data = datasubset.reshape(-1, 2)
 
     # Create an array of tuples representing (I, Q) values
     iq_tuples = [tuple(iq_pair) for iq_pair in iq_data]
 
     return iq_tuples
 
+
 # Example usage
 if __name__ == "__main__":
-    filename = "../CLEAR.bin"  # Replace with the actual file path
-    numberIQSamples=40 # Replace with the number of IQ samples you want to read
-    iq_values = read_iq_values_from_binary_file(filename,numberIQSamples*2)
-    print(iq_values)
+    filename = "../LTE_1M.bin"  # Replace with the actual file path
+    numberIQSamples=40 # Replace with the number of IQ samples you want to read.
+    iq_samples = read_iq_values_from_binary_file(filename,numberIQSamples)
+    print(iq_samples)
